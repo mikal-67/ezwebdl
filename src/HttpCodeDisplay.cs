e5d0d0d2fc25100106cb3df755550a1055acd54f
Mikal <72887058+mikal-67@users.noreply.github.com>
2021-01-17 14:50:25 -0600

    Added source code

---
+++ src/HttpCodeDisplay.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientTest
+{
+    partial class Program
+    {
+        static void CheckCode(int code)
+        {
+            Console.Write($"Got an HTTP {code} response to request ");
+            if (code.ToString()[0] == '2')
+                Console.Write("(Successful, ");
+            else if (code.ToString()[0] == '3')
+                Console.Write("(Redirection, ");
+            else if (code.ToString()[0] == '4')
+                Console.Write("(Client Error, ");
+            else if (code.ToString()[0] == '5')
+                Console.Write("(Server Error, ");
+            else if (code.ToString()[0] == '1')
+                Console.Write("(Information, ");
+            switch (code)
+            {
+                case 100:
+                    Console.Write("Continue");
+                    break;
+                case 101:
+                    Console.Write("Switching Protocols");
+                    break;
+                case 103:
+                    Console.Write("Checkpoint");
+                    break;
+                case 200:
+                    Console.Write("OK");
+                    break;
+                case 201:
+                    Console.Write("Created Content");
+                    break;
+                case 205:
+                    Console.Write("Retry Download");
+                    break;
+                case 301:
+                    Console.Write("Moved");
+                    break;
+                case 302:
+                    Console.Write("Moved Temporarily");
+                    break;
+                case 303:
+                    Console.Write("See Different Website");
+                    break;
+                case 400:
+                    Console.Write("Bad Syntax");
+                    break;
+                case 404:
+                    Console.Write("Page Not Found");
+                    break;
+                case 500:
+                    Console.Write("Generic");
+                    break;
+                case 503:
+                    Console.Write("Server Down");
+                    break;
+                default:
+                    Console.Write("HTTP Status Code Not Recognised");
+                    break;
+            }
+            Console.WriteLine(")\n");
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,78 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientTest
+{
+    partial class Program
+    {
+        // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
+        static readonly HttpClient client = new HttpClient();
+        static HttpResponseMessage response;
+        static async Task Main(string[] args)
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions
+            try
+            {
+                if(args[0] == "/h:code")
+                {
+                    Console.WriteLine("Most Common HTTP Codes:");
+                    Console.WriteLine("404 - Page Not Found");
+                    Console.WriteLine("This means that the page does not exist at the URL.");
+                    Console.WriteLine("200 - OK");
+                    Console.WriteLine("This means that everything is OK and downloading was a success.");
+                    Console.WriteLine("500 - Generic Server Error");
+                    Console.WriteLine("This means that something it wrong at the server's end, and the server gave no further info.");
+                    Console.WriteLine("HTTP Status Code Not Recognized");
+                    Console.WriteLine("This means that a code came that this program could not recognize.");
+                    return;
+                }
+                else if(args[0] == "/h")
+                {
+                    Console.WriteLine("Usage: ezwebdl URL");
+                    Console.WriteLine("  where URL is the URL of the website you want to connect to.");
+                    Console.WriteLine("or: ezwebdl /h");
+                    Console.WriteLine("  Shows this.");
+                    Console.WriteLine("or: ezwebdl /h:code");
+                    Console.WriteLine("  Shows a list of common HTTP codes.");
+                    return;
+                }
+                response = await client.GetAsync(args[0]);
+                CheckCode((int)response.StatusCode);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                string[] s = args[0].Split('/');
+                string currentDir = s[2];
+                Directory.CreateDirectory(currentDir);
+                if(!(s[s.Length - 1].EndsWith(".html") || s[s.Length - 1].EndsWith(".htm") || s[s.Length - 1].EndsWith(".php") || s[s.Length - 1].EndsWith(".asp") || s[s.Length - 1].EndsWith(".aspx")))
+                {
+                    File.WriteAllText(currentDir + "\\" + s[s.Length - 1] + ".html", responseBody);
+                }
+                else
+                {
+                    File.WriteAllText(currentDir + "\\" + s[s.Length - 1], responseBody);
+                }
+            }
+            catch (HttpRequestException)
+            {
+                Console.WriteLine("Page downloading failed");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Usage: ezwebdl URL");
+                Console.WriteLine("  where URL is the URL of the website you want to connect to.");
+                Console.WriteLine("or: ezwebdl /h");
+                Console.WriteLine("  Shows this.");
+                Console.WriteLine("or: ezwebdl /h:code");
+                Console.WriteLine("  Shows a list of common HTTP codes.");
+            }
+            
+        }
+        
+    }
+}
